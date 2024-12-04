1f7cb6823f679ca8a51890aa139de74fd32037c2
Miguel Angel Aviles Garcia <miaviles@c2r16s1.42madrid.com>
2024-12-04 15:39:26 +0100

    test branch

--- GNL/get_next_line_bonus.c
+++ GNL/get_next_line_bonus.c
@@ -6,25 +6,28 @@
 /*   By: miaviles <miaviles@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 12:00:18 by miaviles          #+#    #+#             */
-/*   Updated: 2024/11/13 12:18:14 by miaviles         ###   ########.fr       */
+/*   Updated: 2024/11/13 19:19:30 by miaviles         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
-static char	*read_and_append(int fd, char *remainder, char *buffer)
+static char	*read_and_append(int fd, char *remainder)
 {
 	ssize_t	bytes_read;
 	char	*temp;
+	char	*buffer;
 
+	buffer = (char *)malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
 	bytes_read = 1;
-	while (!ft_strchr(remainder, '\n') && bytes_read != 0)
+	while (!ft_strchr(remainder, '\n') && bytes_read > 0)
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		if (bytes_read == -1)
+		if (bytes_read < 0)
 		{
-			free(remainder);
-			return (NULL);
+			return (free(buffer), free(remainder), NULL);
 		}
 		buffer[bytes_read] = '\0';
 		temp = remainder;
@@ -33,33 +36,22 @@ static char	*read_and_append(int fd, char *remainder, char *buffer)
 		if (!remainder)
 			return (NULL);
 	}
-	return (remainder);
-}
-
-static char	*read_and_fill_buffer(int fd, char *remainder, char *buffer)
-{
-	if (!remainder)
-	{
-		remainder = (char *)malloc(1);
-		remainder[0] = '\0';
-		if (!remainder)
-			return (NULL);
-	}
-	remainder = read_and_append(fd, remainder, buffer);
+	free(buffer);
 	return (remainder);
 }
 
 static char	*extract_line(char *remainder)
 {
 	char	*line;
-	ssize_t	len;
+	char	*newline_pos;
+	size_t	len;
 
 	if (!remainder || *remainder == '\0')
 		return (NULL);
-	line = ft_strchr(remainder, '\n');
-	if (line)
+	newline_pos = ft_strchr(remainder, '\n');
+	if (newline_pos)
 	{
-		len = (line + 1) - remainder;
+		len = (size_t)(newline_pos - remainder) + 1;
 		line = ft_substr(remainder, 0, len);
 	}
 	else
@@ -77,6 +69,8 @@ static char	*update_remainder(char *remainder)
 	char	*tmp;
 	char	*updated_remainder;
 
+	if (!remainder)
+		return (NULL);
 	tmp = ft_strchr(remainder, '\n');
 	if (tmp)
 	{
@@ -96,16 +90,11 @@ static char	*update_remainder(char *remainder)
 char	*get_next_line(int fd)
 {
 	static char	*remainder[MAX_FD];
-	char		*buffer;
 	char		*line ;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!buffer)
-		return (NULL);
-	remainder[fd] = read_and_fill_buffer(fd, remainder[fd], buffer);
-	free(buffer);
+	remainder[fd] = read_and_append(fd, remainder[fd]);
 	if (!remainder[fd])
 		return (NULL);
 	line = extract_line(remainder[fd]);
@@ -113,8 +102,9 @@ char	*get_next_line(int fd)
 	return (line);
 }
 
-/*int main() {
-    int fd1 = open("hola.txt", O_RDONLY);
+/*#include <stdio.h>
+int main() {
+    int fd1 = open("empty.txt", O_RDONLY);
     int fd2 = open("otro.txt", O_RDONLY); 
     
     if (fd1 < 0 || fd2 < 0) {

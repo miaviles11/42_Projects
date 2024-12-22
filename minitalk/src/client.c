14ee1282d8786e4c0e4ceb1e11e57e40db30f641
miaviles11 <mi.aviles.garcia@gmail.com>
2024-12-22 03:14:27 +0100

    Fixed Corruption

--- minitalk/src/client.c
+++ minitalk/src/client.c
@@ -12,60 +12,72 @@
 
 #include "minitalk.h"
 
-static int	validate_pid(char *pid_str)
-{
-	int	i;
+static volatile sig_atomic_t	g_ack_received = 0;
 
-	i = 0;
-	while (pid_str[i])
-	{
-		if (!ft_isdigit(pid_str[i]))
-			return (0);
-		i++;
-	}
-	return (1);
+static void	ack_handler(int sig)
+{
+    (void)sig;
+    g_ack_received = 1;
 }
 
-static void	send_char(int pid, unsigned char c, int delay)
+static int	validate_pid(char *pid_str)
 {
-	int	bit;
+    int	i;
 
-	bit = 8;
-	while (bit--)
-	{
-		if ((c >> bit) & 1)
-			kill(pid, SIGUSR2);
-		else
-			kill(pid, SIGUSR1);
-		usleep(delay);
-	}
+    i = 0;
+    while (pid_str[i])
+    {
+        if (!ft_isdigit(pid_str[i]))
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
+static void	send_char(int pid, unsigned char c)
+{
+    int	bit;
+
+    bit = 8;
+    while (bit--)
+    {
+        g_ack_received = 0;
+        if ((c >> bit) & 1)
+            kill(pid, SIGUSR2);
+        else
+            kill(pid, SIGUSR1);
+        while (!g_ack_received)
+            ;
+    }
 }
 
 static void	send_message(int pid, char *message)
 {
-	int	delay;
-	int	factor;
+    int	i;
 
-	factor = 2;
-	delay = 1000 + (ft_strlen(message) * factor);
-	while (*message)
-		send_char(pid, *message++, delay);
-	send_char(pid, '\0', delay);
+    i = 0;
+    while (message[i])
+    {
+        send_char(pid, message[i]);
+        i++;
+    }
+    send_char(pid, '\0');
 }
 
 int	main(int argc, char **argv)
 {
-	int	pid;
+    int	pid;
 
-	if (argc != 3 || !ft_strlen(argv[2]) || !validate_pid(argv[1]))
-	{
-		ft_putstr_fd("\x1b[31mERROR!\x1b[0m\n", 2);
-		ft_putstr_fd("Formato: ./client [server_pid] \"Mensaje\"\n", 2);
-		return (1);
-	}
-	pid = ft_atoi(argv[1]);
-	ft_printf("Enviando mensaje al servidor (PID: %d)...\n", pid);
-	send_message(pid, argv[2]);
-	ft_printf("Mensaje enviado correctamente.\n");
-	return (0);
+    if (argc != 3 || !ft_strlen(argv[2]) || !validate_pid(argv[1]))
+    {
+        ft_putstr_fd("\x1b[31mERROR!\x1b[0m\n", 2);
+        ft_putstr_fd("Formato: ./client [server_pid] \"Mensaje\"\n", 2);
+        return (1);
+    }
+    pid = ft_atoi(argv[1]);
+    signal(SIGUSR1, ack_handler);
+    ft_printf("Enviando mensaje al servidor (PID: %d)...\n", pid);
+    send_message(pid, argv[2]);
+    ft_printf("Mensaje enviado correctamente.\n");
+    return (0);
 }
